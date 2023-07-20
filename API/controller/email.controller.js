48f6024ad215abeb16f978d9cbbf61bcefc23ef7
Satyamm2 <132651048+Satyamm2@users.noreply.github.com>
2023-07-20 14:41:52 +0530

    Update email.controller.js

--- API/controller/email.controller.js
+++ API/controller/email.controller.js
@@ -5,13 +5,13 @@ export function sendMail(email,password)
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'vilekhofficial@gmail.com',
-    pass: 'rgwjaulxcvdeaoli'
+    user: 'smsatyam28@gmail.com',
+    pass: 'fqqglyntoqvhbvwb'
   }
 });
 
 var mailOptions = {
-  from: 'vilekhofficial@gmail.com',
+  from: 'smsatyam28@gmail.com',
   to: email,
   subject: 'Verification mail PMS',
   html: "<h1>Welcome To Project Management System</h1>You have successfully registers to our site , your login credentials are attached below<h3>Username : "+email+"</h3><h3>Password : "+password+"</h3><h2>Click on the link below to verfiy your account</h2>http://localhost:3000/verifyuser/"+email
