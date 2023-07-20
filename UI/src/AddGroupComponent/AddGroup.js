eb43be2ae1fd9fa6379054324da4ff34866400a0
Satyamm2 <smsatyam28@gmail.com>
2023-07-20 13:28:00 +0530

    first commit

---
+++ UI/src/AddGroupComponent/AddGroup.js
@@ -0,0 +1,22 @@
+import './AddGroup.css';
+
+function AddGroup() {
+
+  return (
+    <>
+    {/* About Start */}
+         <div class="container-fluid bg-secondary p-0">
+          <div class="row g-0">
+              <div class="col-lg-12 py-6 px-5">
+<h1 class="display-5 mb-4">Add Group Here!!!</h1>
+
+
+              </div>
+          </div>
+      </div>
+      {/* About End */}
+     </>		
+  );
+}
+
+export default AddGroup;
---
+++ UI/src/apiurls.js
@@ -0,0 +1,3 @@
+export const apiurluser="http://localhost:3001/user/";
+
+export const apiurlproject="http://localhost:3001/project/";
